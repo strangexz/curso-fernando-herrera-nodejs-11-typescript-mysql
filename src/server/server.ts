baeef2611d8fccb2455015150f06b38d3edc533d
Alberto Figueroa <ja_figueroa@outlook.es>
2020-11-03 19:37:46 -0600

    jfigueroa|03112020 - agregado en index y el server

---
+++ src/server/server.ts
@@ -0,0 +1,30 @@
+import express = require('express');
+import path = require('path');
+
+export default class Server {
+    public app: express.Application;
+    public port: number;
+
+    constructor(port: number) {
+        this.port = port;
+        this.app = express();
+    }
+
+    /* Iniciamos una sola instancia del server */
+    static init(port: number) {
+        return new Server(port);
+    }
+
+    /* Definindo la ruta de la carpeta pública*/
+    private publicFolder() {
+        const publicPath = path.resolve(__dirname, '../public/');
+
+        this.app.use(express.static(publicPath));        
+    }
+
+    /* Arrancando la aplicación */
+    start(callback: () => void) {
+        this.app.listen(this.port, callback);
+        this.publicFolder();
+    }
+}
